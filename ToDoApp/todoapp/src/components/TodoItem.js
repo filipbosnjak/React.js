e3b8b535799215f15bf1bdff2d74e6209fc4977a
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-09-29 21:29:16 +0200

    Using props inside of JSX tags

--- ToDoApp/todoapp/src/components/TodoItem.js
+++ ToDoApp/todoapp/src/components/TodoItem.js
@@ -7,8 +7,8 @@ class TodoItem extends Component {
     render() {
         return (
             <div className="todo">
-                <input type="checkbox" id="do"/>
-                <label htmlFor="do">
+                <input type="checkbox" id={`${this.props.todo.id}`}/>
+                <label htmlFor={`${this.props.todo.id}`}> {/* We can pass class names or whatever we want inside JSX tags like this */}
                     <i className="fas fa-check"></i>
                     <span className="label">{this.props.todo.title}</span>{/* todo objects are being passed here as props and we can use them like this*/}
                 </label> 
