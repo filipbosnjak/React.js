29b4ddbdad81ab96d2e34a74ced95cd3bb591c42
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-10-01 18:21:23 +0200

    useState

--- ReactHooks/react-hooks/src/App.js
+++ ReactHooks/react-hooks/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import './App.css';
 
-function App() {
+
+/*
+function timeExpensiveFunction() {
+  
+}
+*/
+const App = () => {
+
+  const [count, setCount] = useState(10)  /* () => timeExpensiveFunction(); We call it like so cause now the func is called on only first rendering*/
+
   return (
     <div className="App">
       <div>Introducing React Hooks</div>
+      <div>{count}</div>
     </div>
   );
 }
