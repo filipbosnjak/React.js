c3526acefb1fba316f7df67d71ecec1ace504dd2
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-10-04 17:01:02 +0200

    Adding state

--- MultiStepForm/multistep-form/src/components/Root.js
+++ MultiStepForm/multistep-form/src/components/Root.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react'
 import UserDetails from './UserDetails'
 import PersonalDetails from './PersonalDetails';
 import Confirm from './Confirm';
-import Success from './Success'
+import Success from './Success';
+
+
 
 class Root extends Component {
+
+    state = {
+        step:1,
+        firstName:'',
+        lastName:'',
+        email:'',
+        occupation:'',
+        city:'',
+        bio:''
+    }
+
+    next = () => {
+        const {step} = this.state;
+        this.setState({step:step+1});
+    }
+
+    prev = () => {
+        const {step} = this.state;
+        this.setState({step:step-1});
+    }
+
+    handleChange = (input,event) => {
+        this.setState({[input]:event.target.value});
+        //this.setState({[event.target.name]:event.target.value})
+    }
+
     render() {
+        const {step} = this.state;
+        const {firstName,lastName,email,occupation,city,bio} = this.state;
+        const values = {firstName,lastName,email,occupation,city,bio} //We create an object like this firstName,.. = firstName:firstName
         return (
             <div>
                 
