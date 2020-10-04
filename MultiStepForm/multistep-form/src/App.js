a01a8f85a48b893981ba21f88513a964e3c2439e
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-10-04 16:49:17 +0200

    Made blank components and conected them to root

--- MultiStepForm/multistep-form/src/App.js
+++ MultiStepForm/multistep-form/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Root from './components/Root'
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+
+      <Root/>
+      
     </div>
   );
 }
---
+++ MultiStepForm/multistep-form/src/components/Root.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import UserDetails from './UserDetails'
+import PersonalDetails from './PersonalDetails';
+import Confirm from './Confirm';
+import Success from './Success'
+
+class Root extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Root;
