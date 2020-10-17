f065cf695af0bba26e693a4cee3530b6fda723eb
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-10-17 15:59:32 +0200

    Router practice - match and history prop

--- React Router/react-router/src/App.js
+++ React Router/react-router/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {BrowserRouter,Route} from "react-router-dom";
-import {Home , About} from './Components'; //Without currly braces when working with export default, We put {} around the name when we are working with export
+import {BrowserRouter,Route,Switch} from "react-router-dom";
+import {Home , About , Contact, Posts} from './Components'; //Without currly braces when working with export default, We put {} around the name when we are working with export
 import './App.css'
 
 const App = () => {
@@ -9,8 +9,13 @@ const App = () => {
   return (
     <div className="app">
     <BrowserRouter>   {/*Without exact on home rendered just home but on about rendered home and about */}
-      <Route path="/"  component={Home}/>{/*React will render all the routes that match. / and /About match but if we put exact it tells it they dont actually match */}
-      <Route path  ="/about" component={About}/>
+      <Switch> {/*Switch makes sure that only one route is rendered at once. Renders first one that matches*/}
+        <Route path="/" exact component={Home}/>{/*React will render all the routes that match. / and /About match but if we put exact it tells it they dont actually match */}
+        <Route path  ="/about" exact component={About}/>
+        <Route path = '/contact' exact component={Contact}/>
+        <Route path = '/posts/:id' exact component={Posts}/>
+        <Route path = '/' render={() => <div>404</div>}/> {/*Renders only if none of the routes is matched */}
+      </Switch>
     </BrowserRouter>
     </div>
   );
--- React Router/react-router/src/Components.js
+++ React Router/react-router/src/Components.js
@@ -1,18 +1,52 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-export const Home = () => {
+export const Home = ({location , match , history}) => { //Passed props from Route
+    /*
+    console.log(match);
+    console.log(history);
+    console.log(location);
+    */
+
     return (
-        <div>
+        <div className="home">
             Home
+            <Link className="link" to='/about'>Go to about</Link>
         </div>
     );
 }
 
-export const About = () =>{
+export const About = ({history}) =>{
     return(
-        <div>
+        <div className="about">
             About
+            <button onClick = {() => {
+                history.push('/contact');
+            }}> Go to contact</button>
         </div>
     )
 }
 
+export const Contact = ({history}) => {
+    return(
+        <React.Fragment>
+            <div>Contact Me</div>
+            <button onClick={() => {
+                history.push('/posts/:id');
+            }}>Go to posts</button>
+        </React.Fragment>
+    );
+}
+
+export const Posts = ({match}) => {
+    const check = (id) => {
+        let num = id
+        console.log(num)
+        num === ':id' ? num = '/' : num = id
+        return num; 
+    }
+ 
+    return(
+    <div>Rendering post {check(match.params.id)} </div>
+    )
+}
